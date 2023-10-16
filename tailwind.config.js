38dfec76dd6c08868ec3e549f8675e13e0c62b02
Tec94 <caodangkhiet1@gmail.com>
2023-10-16 10:28:36 -0500

    install tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./my-app/static/*.{js,tsc,ts}", "./my-app/templates/*.{html}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
